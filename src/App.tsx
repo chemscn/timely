a7fb082e534e69df92b277a5c24f7d35d0d0e8c9
unknown <chemdev233@gmail.com>
2022-01-07 21:31:58 -0500

    Added react material-ui, icons, and added a chip to make sure it works

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import React from "react";
-
+import Chip from "@mui/material/Chip";
+import DeleteIcon from "@mui/icons-material/Delete";
 function App() {
   return (
     <div className="App">
       <h1>Timely :)</h1>
+      <Chip
+        color="error"
+        label="Custom delete icon"
+        deleteIcon={<DeleteIcon />}
+        onDelete={() => console.log("delete")}
+      />
     </div>
   );
 }
